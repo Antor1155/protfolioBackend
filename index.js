95c3f3da6a7564da172773dfedc322f452996a81
Md Antor <md.antor1155@gmail.com>
2022-11-06 16:58:08 +0600

    added nodemailer package to send mail direct  form server

--- index.js
+++ index.js
@@ -12,11 +12,37 @@ app.get("/",(req, res)=>{
 })
 
 app.post("/email",(req, res)=>{
-    console.log("req is:" + req.body.obj);
-    console.log("req is:" + req.hostname);
+    console.log(req.body.email);
     res.send( {obj:"this is response send to the website back"});
 })
 
 app.listen(port, ()=>{
     console.log("this port is listening to " + port)
+});
+
+
+// this part is to send mail to my gmail 
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'md.antor1155job@gmail.com',
+    pass: 'antor1155007073'
+  }
+});
+
+var mailOptions = {
+  from: 'youremail@gmail.com',
+  to: 'myfriend@yahoo.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
 });
