e6485838086fa70c8891efae9d126b19cbb26750
Md Antor <md.antor1155@gmail.com>
2022-11-06 15:32:14 +0600

    fixed bug with cors ,, should be cors()

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 
 app.get("/",(req, res)=>{
-    res.send("running server")
+    res.send("res from app .get" )
+})
+
+app.post("/email",(req, res)=>{
+    console.log("req is:" + req.body.obj);
+    console.log("req is:" + req.hostname);
+    res.send( {obj:"this is response send to the website back"});
 })
 
 app.listen(port, ()=>{
